a9c92173eb3b95353c25872efd1351707467f81f
Vitor <mcid.vitor@gmail.com>
2020-02-19 15:55:26 -0300

    add some improvements to post-management

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,19 +3,60 @@ import React, { Component } from 'react';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+import axios from 'axios';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false 
+    }
+
+    componentDidMount (){
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts: updatedPosts});
+                // console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({error: true});
+            });           
+
+    }
+
+    postSelectedHandler = (id) =>{
+        this.setState({selectedPostId: id});
+    } 
+
     render () {
+        let posts = <p style={{textAlign: 'center'}}> Something went Wrong?</p>;
+        if (!this.state.error){
+            posts = this.state.posts.map(post => {
+            return <Post
+                     key={post.id}
+                     title={post.title}
+                     author = {post.author}
+                     clicked= {()=> this.postSelectedHandler(post.id)}
+                   />;
+            });
+        }
+      
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
